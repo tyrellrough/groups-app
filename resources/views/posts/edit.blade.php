378b2d418bc81dc65eee2a48e982e7190130cf8e
Tyrell <2011077@swansea.ac.uk>
2023-04-18 13:17:50 +0100

    User's posts show when clicked on user. Conditional edit button added on posts.

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,28 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <section id="newPostSection">
+      <form method="post" action="{{ route('posts.store') }}">
+        @csrf
+        <label><h4>Text</h4><input type="text" name="text" maxlength="1000" required></label>
+        <input type="hidden" name="pageID" value="{{ $page->id  }}">
+        <input type="hidden" name="groupID" value="{{ $group->id  }}">
+        <input type="submit" value="Submit">
+      </form>
+    
+  </section>
+        
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
